7a43a0e7ae07c3a5ad8cc7976bb590437cdee281
Brian Bantigue <bignatue@gmail.com>
2020-06-24 21:20:12 -0400

    First Commit

---
+++ src/build/precache-manifest.373cb8e86917d6769a0f8d66774d5f89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4eb1ddb1697dfe319fb6cee6dff5ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f00acc68063436beaa30",
+    "url": "/static/css/main.fbbf06c3.chunk.css"
+  },
+  {
+    "revision": "5e93f75bb679d3208105",
+    "url": "/static/js/2.17d6d19f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.17d6d19f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00acc68063436beaa30",
+    "url": "/static/js/main.0c2fc7da.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
